3b1a2d645d03a9fcff158a52ee60f63980969380
Christian Elena <christianelena92@gmail.com>
2026-01-25 10:52:07 -0600

    solution for SpareArray usingHashMaps

--- HackerRankAlgos/Arrays/sparseArray.js
+++ HackerRankAlgos/Arrays/sparseArray.js
@@ -9,3 +9,28 @@
  *  2. STRING_ARRAY queries
  * 
  */
+
+function matchingStrings(stringList, queries) {
+let freq = new Map();
+
+//1. Iterate stringList build word count
+//- build frequency Map
+stringList.forEach((w) => {
+    const curr = freq.get(w) || 0;
+    freq.set(w, curr + 1);
+})
+
+//2. Iterate queries
+//   - lookup each query in Map
+//   - push count or 0
+let res = queries.map(w => {
+    let c = freq.get(w) || 0;
+    return c;
+})
+
+//3. Return results
+return res;
+}
+const stringList = ["aba", "baba", "aba", "xzxb"];
+const queries = ["aba", "xzxb", "ab"];
+console.log(matchingStrings(stringList,queries))
