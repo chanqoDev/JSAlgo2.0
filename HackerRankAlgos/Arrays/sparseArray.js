d14a0aa2b5a566605743e231ef35f75b9e89ebc2
Christian Elena <christianelena92@gmail.com>
2026-01-25 09:22:51 -0600

    hackerrank: Sparese Array

---
+++ HackerRankAlgos/Arrays/sparseArray.js
@@ -0,0 +1,10 @@
+/**
+ * There is a collection of input strings and a collection of query strings. 
+ * For each query string, determine how many times it occurs in the list of input strings.
+ *  Return an array of the results.
+ * 
+ *  * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY stringList
+ *  2. STRING_ARRAY queries
+ */
