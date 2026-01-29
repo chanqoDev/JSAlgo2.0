8f3fa534b484a5bea93351e7e6799251aa5be758
Christian Elena <christianelena92@gmail.com>
2026-01-28 19:56:20 -0600

    HackerRank ArrayManinpulation solve

--- HackerRankAlgos/Arrays/array_Manipulation.js
+++ HackerRankAlgos/Arrays/array_Manipulation.js
@@ -4,6 +4,10 @@
  * Starting with a 1-indexed array of zeros and a list of operations, 
  * for each operation add a value to each array element between two given indices, inclusive. 
  * Once all operations have been performed, return the maximum value in the array.
+ * 
+ * We use a difference array where:
+diff[a] += k → “from here on, values go up by k”
+diff[b + 1] -= k → “from here on, stop adding k”
  */
 
 const n = 10;
@@ -13,8 +17,27 @@ const queries = [
   [6, 9, 1]
 ];
 
+// Each query says:
+// Add k to every element from index a to index b (inclusive).
+// create the diff array and place markers 
+// We use a difference array where:
+// diff[a] += k → “from here on, values go up by k”
+// diff[b + 1] -= k → “from here on, stop adding k”
 function arrayManipulation(n, queries) {
+const diff = new Array(n + 2).fill(0); 
+let running = 0; 
+let max = 0;
 
+for (const [a, b, k] of queries){
+diff[a] += k; 
+diff[b + 1] -= k; 
+}
+
+for (let i = 1; i <= n; i++) {
+      running += diff[i];
+      if (running > max) max = running; 
+ }
+return max; 
 }
 
 console.log(arrayManipulation(n, queries)); // 10
