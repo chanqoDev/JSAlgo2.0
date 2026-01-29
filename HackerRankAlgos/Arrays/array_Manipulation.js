d8c7b45fa39a57adedbb5b5bb3e79d8513cc477b
Christian Elena <christianelena92@gmail.com>
2026-01-28 18:58:27 -0600

    Hackerrank Array Manipulation

---
+++ HackerRankAlgos/Arrays/array_Manipulation.js
@@ -0,0 +1,20 @@
+/**
+ * Challnege: Array Manipulation
+ * Range updates using a difference array
+ * Starting with a 1-indexed array of zeros and a list of operations, 
+ * for each operation add a value to each array element between two given indices, inclusive. 
+ * Once all operations have been performed, return the maximum value in the array.
+ */
+
+const n = 10;
+const queries = [
+  [1, 5, 3],
+  [4, 8, 7],
+  [6, 9, 1]
+];
+
+function arrayManipulation(n, queries) {
+
+}
+
+console.log(arrayManipulation(n, queries)); // 10
