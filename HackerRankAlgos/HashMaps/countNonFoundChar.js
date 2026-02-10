900338f302bb44a6ead13624ee446cb22821bf95
Christian Elena <christianelena92@gmail.com>
2026-02-10 01:01:01 -0600

    hashmap solution space:o(n)

--- HackerRankAlgos/HashMaps/countNonFoundChar.js
+++ HackerRankAlgos/HashMaps/countNonFoundChar.js
@@ -1,11 +1,21 @@
 // Given a string, return the number of characters that appear only once
 function countUniqueChar(str) {
 if (!str ) return 0; 
-let freq = {}; 
+// let freq = {}; 
+// for (const char of str) {
+//    freq[char] = (freq[char] || 0) + 1;    
+// }
+let freq = new Map(); 
+let mostFound = []; 
 for (const char of str) {
-   freq[char] = (freq[char] || 0) + 1;    
+    freq.set(char, (freq.get(char) || 0) + 1);
 }
-return freq; 
+for (const val of freq.entries()) {
+if(val[1] > 2){
+    mostFound=val;
+}
+}
+return mostFound; 
 }
 
 
@@ -13,4 +23,4 @@ const str = 'NachoCheese Burrito';
 let r = countUniqueChar(str); 
 let s = countUniqueChar(); 
 console.log(r);
-console.log(s); 
+// console.log(s); 
